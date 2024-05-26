b3e3928bd59733e0c976968cf40685f07259eb9c
Eduardo-correa1 <159133054+Eduardo-correa1@users.noreply.github.com>
2024-05-26 17:23:43 +0000

    agregadas proptypes

--- src/js/component/Card.jsx
+++ src/js/component/Card.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import PropTypes from 'prop-types';
+
 
 
 const Card = ({image, text, description}) => {
     return (
-      <div className="card" style={{ width: '18rem' }}>
+      <div className=" card" style={{ width: '18rem' }}>
         <img src={image} className="card-img-top" alt="..." />
         <div className="card-body">
           <p className="card-text">{text}</p>
@@ -12,4 +14,10 @@ const Card = ({image, text, description}) => {
       </div>
     );
   };
+
+  Card.propTypes = {
+    image: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    }
 export default Card
